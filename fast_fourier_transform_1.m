8e98a1f76cc65e96297ca620bfe2dbd17a8a2e0d
Francis <bup9812@naver.com>
2020-05-04 10:28:57 +0900

    Update fast_fourier_transform_1.m

--- fast_fourier_transform_1.m
+++ fast_fourier_transform_1.m
@@ -1,9 +1,9 @@
->> x = [1 2 3 4];
->> N = length(x);
->> X1 = fft(x,N);
->> x1 = ifft(X1,N);
->> stem(real(x1),'bo')
->> hold on
->> grid on
->> stem(x,'r+')
->> hold off;
+x = [1 2 3 4];
+N = length(x);
+X1 = fft(x,N);
+x1 = ifft(X1,N);
+stem(real(x1),'bo')
+hold on
+grid on
+stem(x,'r+')
+hold off;
