7679166d4fdcfc72b6b7f4dfb9efd861c6d2d93c
Francis <bup9812@naver.com>
2020-06-11 01:10:45 +0900

    Update Noise_Reduction.m

--- FIR_Filter Design/Noise_Reduction.m
+++ FIR_Filter Design/Noise_Reduction.m
@@ -1,9 +1,9 @@
 >> %7.4.1 Noise Reduction
 >> fs = 8000;   %Sampling rate
 >> T = 1/fs;    %Sampling period
-v = sqrt(0.1)*randn(1,250);   			%Generate Gaussian random noise
-n = 0:1:249;
-x = sqrt(2)*sin(2*pi*500*n*T)+v;  			%Generate 500Hz + noise
+>> v = sqrt(0.1)*randn(1,250);   			%Generate Gaussian random noise
+>> n = 0:1:249;
+>> x = sqrt(2)*sin(2*pi*500*n*T)+v;  			%Generate 500Hz + noise
 
 >> freqz(x,1)					%Digital frequency response
 
@@ -25,10 +25,10 @@ x = sqrt(2)*sin(2*pi*500*n*T)+v;  			%Generate 500Hz + noise
 >> f = [0:N/2]*fs/N;
 >> figure(4)						%frequency domain
 >> Axk = 2*abs(fft(x))/N;Axk(1)=Axk(1)/2;  	
-subplot(2,1,2); plot(f,Axk(1:N/2+1));
-xlabel('Frequency (Hz)'); ylabel('Amplitude |X(f)|');grid;
-subplot(2,1,1); plot(f,Axk(1:N/2+1));
-xlabel('Frequency (Hz)'); ylabel('Amplitude |X(f)|');grid;
-Ayk = 2*abs(fft(outlo))/N;Ayk(1) = Ayk(1)/2;
-subplot(2,1,2);plot(f,Ayk(1:N/2+1)); 
-xlabel('Frequency (Hz)'); ylabel('Amplitude |Y(f)|');grid;
+>> subplot(2,1,2); plot(f,Axk(1:N/2+1));
+>> xlabel('Frequency (Hz)'); ylabel('Amplitude |X(f)|');grid;
+>> subplot(2,1,1); plot(f,Axk(1:N/2+1));
+>> xlabel('Frequency (Hz)'); ylabel('Amplitude |X(f)|');grid;
+>> Ayk = 2*abs(fft(outlo))/N;Ayk(1) = Ayk(1)/2;
+>> subplot(2,1,2);plot(f,Ayk(1:N/2+1)); 
+>> xlabel('Frequency (Hz)'); ylabel('Amplitude |Y(f)|');grid;
